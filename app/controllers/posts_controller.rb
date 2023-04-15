2fc23e6dde674cabe47eb3991124781ab1ccfeb1
haliljon <juraboevhaliljon@gmail.com>
2023-04-16 00:08:06 +0500

    Check for linters errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,11 +15,12 @@ class PostsController < ApplicationController
   end
 
   def create
-    @new_post = current_user.posts.create(title: params[:post][:title], text: params[:post][:text], author_id: params[:user_id])
+    @new_post = current_user.posts.create(title: params[:post][:title], text: params[:post][:text],
+                                          author_id: params[:user_id])
     if @new_post.save
-      redirect_to user_posts_path(current_user),  notice: "Post created successfully"
+      redirect_to user_posts_path(current_user), notice: 'Post created successfully'
     else
-      render :new, alert: "Post not created"
-    end  
+      render :new, alert: 'Post not created'
+    end
   end
 end
