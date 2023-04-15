1026766281eb854912ee3b61f469deef8cdfdc40
haliljon <juraboevhaliljon@gmail.com>
2023-04-16 00:07:00 +0500

    Add new like action

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,11 @@
+class LikesController < ApplicationController
+    def create 
+        @like=Like.new(author_id: current_user.id, post_id: params[:post_id])
+        if @like.save
+            redirect_to user_posts_path(current_user)  
+            flash[:success]="Like added"
+        else
+            flash[:error]="Something went wrong"
+        end
+    end
+end
