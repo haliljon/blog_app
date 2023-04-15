8597ba996959d53f5f6d65e19ac14c38f4da6461
haliljon <juraboevhaliljon@gmail.com>
2023-04-15 16:33:41 +0500

    Add database and test the view

--- db/migrate/20230406173054_create_users.rb
+++ db/migrate/20230406173054_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :name, null: false, index: { unique: true }
       t.string :photo
       t.text :bio
-      t.string :posts_counter
+      t.integer :posts_counter, default: 0
 
       t.timestamps
     end
--- db/migrate/20230407091154_change_post_counter_type.rb
+++
@@ -1,5 +0,0 @@
-class ChangePostCounterType < ActiveRecord::Migration[7.0]
-  def change
-    change_column :users, :posts_counter, :integer, using: 'posts_counter::integer'
-  end
-end
