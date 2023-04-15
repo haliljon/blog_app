cdad4f850efbb6ea153c2b77a6db96fb72ce3358
haliljon <juraboevhaliljon@gmail.com>
2023-04-15 22:17:01 +0500

    Add new post action and view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,17 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @comments = @post.comments
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @new_post = current_user.posts.create(title: params[:post][:title], text: params[:post][:text], author_id: params[:user_id])
+    if @new_post.save
+      redirect_to user_posts_path(current_user),  notice: "Post created successfully"
+    else
+      render :new, alert: "Post not created"
+    end  
+  end
 end
